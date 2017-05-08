c2073326f7ae1b73ff00640db40ba39a9e3202f2
foooomio <foooomio@gmail.com>
2017-05-08 18:33:07 +0900

    Get rid of white spaces

--- src/code.js
+++ src/code.js
@@ -1,19 +1,19 @@
 function run() {
   const prop = PropertiesService.getScriptProperties().getProperties();
   const calendar = CalendarApp.getCalendarById(prop.calendar_id);
-  
+
   // Push Gmail messages into an array
   const threads = GmailApp.search('yoyaku@expy.jp subject:新幹線予約内容');
   const messages = threads.reduceRight(function(a, b) {
     return a.concat(b.getMessages());
   }, []);
-  
+
   messages.forEach(function(message) {
     if(message.getDate().getTime() < prop.last_execution) return;
-    
+
     const body = message.getBody().split('変更前の予約内容');
     const reservation = scrape(body[0]);
-    
+
     if(body.length === 2) {
       // Delete the old event
       const before = scrape(body[1]);
@@ -22,7 +22,7 @@ function run() {
       })[0];
       event.deleteEvent();
     }
-    
+
     // Create an event
     calendar.createEvent(
       '🚅' + reservation.train,
@@ -31,17 +31,17 @@ function run() {
       {location: reservation.seat}
     );
   });
-  
+
   PropertiesService.getScriptProperties().setProperty('last_execution', Date.now());
 }
 
 function scrape(text) {
   const year = new Date().getFullYear();
-  
+
   const date = text.match(/乗車日　(\d{1,2})月(\d{1,2})日/);
   const time = text.match(/.+\((\d{1,2}):(\d{1,2})\)→(.+号)→.+\((\d{1,2}):(\d{1,2})\)/);
   const seat = text.match(/\d{1,2}号車\d{1,2}番[A-E]席/);
-  
+
   return {
     train: time[3],
     seat: seat[0],
